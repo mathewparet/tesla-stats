1867b1953d90653cb67824313aea615c23152f45
Mathew Paret <mathewparet@gmail.com>
2024-03-10 22:00:50 +1100

    Bill generation completed

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Notifications\NewBillReady;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use mathewparet\LaravelPolicyAbilitiesExport\Traits\ExportsPermissions;
 
 class Bill extends Model
@@ -24,5 +27,46 @@ class Bill extends Model
     public function billingProfile()
     {
         return $this->belongsTo(BillingProfile::class);
+    }
+
+    public static function booted()
+    {
+        static::created(fn($bill) => $bill->sendBill());
+    }
+
+    public function sendBill()
+    {
+        if($this->to->lte(now()))
+        {
+            $this->billingProfile->team->owner->notify(new NewBillReady($this));
+        }
+    }
+
+    public function energyUsed(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->getCharges()->sum('energy_consumed')
+        );
+    }
+
+    public function totalCost(): Attribute
+    {
+        return new Attribute(
+            get: fn() => $this->getCharges()->sum('cost')
+        );
+    }
+
+    private function getCharges()
+    {
+        $query = Charge::whereIn('vehicle_id', $this->billingProfile->vehicles()->pluck('id'))
+            ->where('started_at', '>=', $this->from->setTimezone($this->billingProfile->setTimezone))
+            ->where('ended_at', '<', $this->to->addDay()->setTimezone($this->billingProfile->setTimezone));
+
+        return tap($query, function($query) {
+            Log::debug('Charges SQL', [
+                'sql' => $query->toSql(),
+                'bindings' => $query->getBindings()
+            ]);
+        });
     }
 }
---
+++ resources/views/emails/NewBillReady.blade.php
@@ -0,0 +1,25 @@
+<x-mail::message>
+# Charging cost for the Billing Profile {{ $bill->billingProfile->name }} is ready
+
+{{ Str::plural('Vehicle', $bill->billingProfile->vehicles->count()) }}:
+<ol>
+    @foreach ($bill->billingProfile->vehicles as $vehicle)
+        <li>{{ $vehicle->name }} / {{ $vehicle->plate }} / {{ $vehicle->masked_vin }}</li>
+    @endforeach
+</ol>
+
+Charging Location: {{ $bill->billingProfile->address }}
+
+Period: {{ $bill->from->format('d-M-Y') }} to {{ $bill->to->format('d-M-Y') }}
+
+Total Energy Used: {{ number_format($bill->energy_used, 2) }} kwh
+
+Total Cost of charging at this location: {{ number_format($bill->total_cost, 2) }}
+
+<x-mail::button :url="$url">
+View Breakdown
+</x-mail::button>
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>
